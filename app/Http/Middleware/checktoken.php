6d2ab32a0fe763a71348174d327b6b8f1f5a0041
= <=>
2019-12-05 01:52:11 +0700

    Tạo Api (Middleware kết hợp cùng với Route, Controller) mà yêu cầu có token gán vào header của request.

---
+++ app/Http/Middleware/checktoken.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class checktoken
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if ($request->header('token')) {
+            return $next($request);
+        }
+    }
+}
