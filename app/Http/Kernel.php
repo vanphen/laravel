dbc59fc446c39787c3a2d7c783b412ff643aad2b
= <=>
2019-12-04 22:52:03 +0700

    checktoken

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -62,6 +62,8 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'hellomidleware' => \App\Http\Middleware\Check_token::class,
+        'checktoken' => \App\Http\Middleware\checktoken::class,
+
     ];
 
     /**
